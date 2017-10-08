17297d155d013d1458d9eb9916ac93b90d8a1803
Rafa_home <bogaveev.rarfael@gmail.com>
2017-10-08 13:25:37 +0300

    Kick off. Added slim dependency

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 08.10.2017
+ * Time: 12:46
+ */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require  '../vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello world ");
+
+    return $response;
+});
+
+$app->run();
